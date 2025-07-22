fa0303d7b9e18ca141bba544ebe7e7835440c4f5
shewitt.au <shewitt.au@gmail.com>
2025-07-23 01:29:45 +1000

    Show garbage conjuring

--- main.cpp
+++ main.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <random>
 
 int main()
 {
     using namespace std;
 
-    vector<int> a{1, 2, 3};
-    vector<int> b{3, 2, 1};
+    vector<int> sorted;
+    vector<int> shuffled;
 
-    if (is_permutation(a.begin(),a.end(), b.begin()))
-        cout << "==" << endl;
-    else
-        cout << "!=" << endl;
+    for (int i=1; i<=100; ++i) {
+        sorted.push_back(i);
+        shuffled.push_back(i);
+    }
+
+    random_device rd;
+    mt19937 g(rd());
+
+    for (;;) {
+        shuffle(shuffled.begin(), shuffled.end(), g);
+
+        sort(shuffled.begin(), shuffled.end(), [](int l, int r) {
+            if (l%5==0 && r%5==0)
+                return true;
+
+            return l<r;
+        });
+
+        if (!is_permutation(sorted.begin(), sorted.end(), shuffled.begin())) {
+            for (auto i=0; i<shuffled.size(); ++i) {
+                if (i!=0) cout << ", ";
+                std::cout << shuffled[i];
+            }
+            
+            return 0;
+        }
+    }
 
     return 0;
 }
